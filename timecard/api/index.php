2358489e6a91dc3b9ffd55248180a0e96f21c326
tknr <geishagirlskoume@gmail.com>
2021-08-04 07:34:44 +0900

    modified:   timecard/api/index.php

--- timecard/api/index.php
+++ timecard/api/index.php
@@ -1,3 +1,7 @@
 <?php
-phpinfo();
+$json = file_get_contents("php://input");
+$contents = json_decode($json, true);
+
+header("Access-Control-Allow-Origin: *");header("Access-Control-Allow-Origin: *");
+echo json_encode('ok');
 ?>
