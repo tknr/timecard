b6116be095bbb6e11dec652ee523ab46fad404cb
tknr <geishagirlskoume@gmail.com>
2021-08-05 11:19:32 +0900

    modified:   src/assets/css/main.css 	modified:   src/assets/js/main.js 	modified:   timecard/assets/css/main.min.css 	modified:   timecard/assets/js/main.min.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,14 @@
+function showResult(text,alert = false){
+	var element = document.getElementById("result");
+	if(alert){
+		element.classList.add( "alert" ) ;
+	}else{
+		element.classList.remove( "alert" ) ;
+	}
+
+	element.innerHTML = text;
+}
+
 function setLocation(pos){
 
 	// 緯度・経度を取得
@@ -26,12 +37,11 @@ function setLocation(pos){
 	xhr.onload = function(){        //レスポンスを受け取った時の処理（非同期）
 		var res = xhr.responseText;
 		if (res.length>0){
-			console.log(res);
-			document.getElementById("result").innerHTML = res;
+			showResult(res);
 		}
 	};
 	xhr.onerror = function(){       //エラーが起きた時の処理（非同期）
-		alert("error!");
+		showResult("error!",true);
 	}
 	xhr.open('post', "api/", true);    //(1)
 	xhr.setRequestHeader('Content-Type', 'application/json');
@@ -41,10 +51,10 @@ function setLocation(pos){
 // エラー時に呼び出される関数
 function showErr(err){
 	switch(err.code){
-		case 1 : alert("位置情報の利用が許可されていません"); break;
-		case 2 : alert("デバイスの位置が判定できません"); break;
-		case 3 : alert("タイムアウトしました"); break;
-		default : alert(err.message);
+		case 1 : showResult("位置情報の利用が許可されていません",true); break;
+		case 2 : showResult("デバイスの位置が判定できません",true); break;
+		case 3 : showResult("タイムアウトしました",true); break;
+		default : showResult(err.message);
 	}
 }
 
@@ -58,7 +68,7 @@ function postApi() {
 		};
 		navigator.geolocation.getCurrentPosition(setLocation, showErr, opt);
 	}else{
-		alert("ブラウザが位置情報取得に対応していません");
+		showResult("ブラウザが位置情報取得に対応していません",true);
 	}
 }
 
