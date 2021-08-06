ac05a9fbf3959f900a07d11741d05a768dc20ebe
tknr <geishagirlskoume@gmail.com>
2021-08-06 15:29:48 +0900

    modified:   timecard/api/index.php

--- timecard/api/index.php
+++ timecard/api/index.php
@@ -3,12 +3,12 @@ $json = file_get_contents("php://input");
 $contents = json_decode($json, true);
 
 $record = [$contents['date'],$contents['lat'],$contents['lon']];
-$file_path = __DIR__.'../../../data/timecard.tsv';
+$file_path = __DIR__.'../../../data/timecard_'.date('Ym').'.tsv';
 $handle = fopen($file_path, "ab");
 fputcsv($handle, $record, "\t");
 fclose($handle);
 
 $res = ['ok'];
-header("Access-Control-Allow-Origin: *");header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 echo json_encode($res);
 ?>
