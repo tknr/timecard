a9eb04e246d7e9e2f2953b0912ca6c06950912ea
tknr <geishagirlskoume@gmail.com>
2021-08-16 21:48:21 +0900

    Changes to be committed: 	modified:   minify.sh 	renamed:    timecard/api/index.php -> public/api/index.php 	renamed:    timecard/assets/css/main.min.css -> public/assets/css/main.min.css 	renamed:    timecard/assets/css/reset.min.css -> public/assets/css/reset.min.css 	renamed:    timecard/assets/js/main.min.js -> public/assets/js/main.min.js 	renamed:    timecard/index.html -> public/index.html

--- minify.sh
+++ minify.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 cd `dirname $0`
 
-mkdir -p timecard/assets/js
-mkdir -p timecard/assets/css
-mkdir -p timecard/images
+mkdir -p public/assets/js
+mkdir -p public/assets/css
+mkdir -p public/images
 
 for js in `find src/assets/js/ -name *.js | grep -v min.`;do
-	dst=${js/src/timecard}
+	dst=${js/src/public}
 	dst="${dst%%.js}.min.js"
 	npx uglifyjs --compress --mangle -- ${js} >| ${dst}
 done
 
 for css in `find src/assets/css/ -name *.css | grep -v min.`;do
-	dst=${css/src/timecard}
+	dst=${css/src/public}
 	dst="${dst%%.css}.min.css"
 	npx cleancss ${css} >| ${dst}
 done
 
 for jpg in `find src/images/ -name *.jpg`;do
-	dst=${jpg/src/timecard}
+	dst=${jpg/src/public}
 	cwebp -q 75 -metadata icc -sharp_yuv ${jpg} -o ${dst%%.jpg}.webp
 done
 
 for png in `find src/images/ -name *.png`;do
-	dst=${png/src/timecard}
+	dst=${png/src/public}
 	cwebp -lossless -metadata icc ${png} -o ${dst%%.png}.webp
 done
 
-npx purgecss --css timecard/assets/css/*.css --content timecard/index.html timecard/assets/js/*.js --output timecard/assets/css
+npx purgecss --css public/assets/css/*.css --content public/index.html public/assets/js/*.js --output public/assets/css
